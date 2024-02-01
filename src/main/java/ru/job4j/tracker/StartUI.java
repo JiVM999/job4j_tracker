686d678761df5313f2536d9e6c902ab0567cc033
Kolbaskakapchenaya <81241405+Kolbaskakapchenaya@users.noreply.github.com>
2024-02-01 17:40:08 +0300

    3.0. Реализация класса StartUI. Вывод меню [#500743 #478326]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -33,6 +33,7 @@ public class StartUI {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Tracker tracker = new Tracker();
+
         new StartUI().init(scanner, tracker);
     }
 }
