267aa689cae8ea48c1bafddf0f2aeba8ff601470
Kolbaskakapchenaya <81241405+Kolbaskakapchenaya@users.noreply.github.com>
2024-03-09 17:44:06 +0300

    2.1. Тест на ValidateInput [#311887]

---
+++ src/test/java/ru/job4j/tracker/ValidateInputTest.java
@@ -0,0 +1,44 @@
+package ru.job4j.tracker;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ValidateInputTest {
+
+    @Test
+    void whenInvalidInput() {
+        Output output = new StubOutput();
+        Input in = new MockInput(
+                new String[] {"one", "1"}
+        );
+        ValidateInput input = new ValidateInput(output, in);
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected).isEqualTo(1);
+    }
+
+    @Test
+    void whenCorrectlyInvalidInput() {
+        Output output = new StubOutput();
+        Input in = new MockInput(
+                new String[] {"3"}
+        );
+        ValidateInput input = new ValidateInput(output, in);
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected).isEqualTo(3);
+    }
+
+    @Test
+    void whenMultipleCorrectlyInvalidInput() {
+        Output output = new StubOutput();
+        Input in = new MockInput(
+                new String[] {"1", "test1", "2"}
+        );
+        ValidateInput input = new ValidateInput(output, in);
+        int start = input.askInt("Enter menu:");
+        assertThat(start).isEqualTo(1);
+        String str = input.askStr("Введите имя: ");
+        assertThat(str).isEqualTo("test1");
+        int finish = input.askInt("Enter menu:");
+        assertThat(finish).isEqualTo(2);
+    }
+}
