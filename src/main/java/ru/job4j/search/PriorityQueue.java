c0e9966241b98e784282b9c066211e32905e7fbf
JiVM999 <mikhail.zenkov.1994@mail.ru>
2024-05-16 18:14:13 +0300

    5. Очередь с приоритетом на LinkedList [#41670 #496379]

--- src/main/java/ru/job4j/search/PriorityQueue.java
+++ src/main/java/ru/job4j/search/PriorityQueue.java
@@ -8,8 +8,8 @@ public class PriorityQueue {
     public void put(Task task) {
         int index = 0;
         for (Task element : tasks) {
-            if (task.getPriority() >= element.getPriority()) {
-                index++;
+            if (task.getPriority() < element.getPriority()) {
+                break;
             }
         }
         this.tasks.add(index, task);
