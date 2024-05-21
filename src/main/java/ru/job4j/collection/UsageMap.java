c09c1b79f8113641ea6bc2770ced48ea8f9559f1
JiVM999 <mikhail.zenkov.1994@mail.ru>
2024-05-21 17:47:20 +0300

    0. Map, HashMap [#212652]

---
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -0,0 +1,17 @@
+package ru.job4j.collection;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UsageMap {
+    public static void main(String[] args) {
+        HashMap<String, String> accounts = new HashMap<>();
+        accounts.put("qwerty@mail.com", "Mikhail Zenkov");
+        accounts.put("asd@gmail.com", "Andrey Lenkov");
+        for (Map.Entry<String, String> entry : accounts.entrySet()) {
+            String key = entry.getKey();
+            String value = entry.getValue();
+            System.out.println(key + " = " + value);
+        }
+    }
+}
