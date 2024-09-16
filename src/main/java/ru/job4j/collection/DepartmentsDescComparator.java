be1b60cf3d477d06daae175cbe64fabe75b1f70a
Kolbaskakapchenaya <81241405+Kolbaskakapchenaya@users.noreply.github.com>
2024-09-16 12:34:13 +0300

    3. Отсортировать департаменты [#34352]

---
+++ src/main/java/ru/job4j/collection/DepartmentsDescComparator.java
@@ -0,0 +1,24 @@
+package ru.job4j.collection;
+
+import java.util.Comparator;
+
+public class DepartmentsDescComparator implements Comparator<String> {
+    @Override
+    public int compare(String left, String right) {
+        int min = Math.min(left.length(), right.length());
+        boolean asc = (left.substring(0, min - 2)
+                .equals(right.substring(0, min - 2)))
+                && min != 2;
+        if (asc) {
+            return new StringCompare().compare(left, right);
+        }
+        for (int i = 0; i < min; i++) {
+            if (left.charAt(i) != right.charAt(i)) {
+                return Character.compare(right.charAt(i), left.charAt(i));
+            }
+        }
+        return Integer.compare(left.length(), right.length());
+    }
+
+
+}
